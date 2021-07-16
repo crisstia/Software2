26033cf0acd8ddc4e9e12fd29b2c80843fd1aabc
cristian <cristian.torres6324@utc.edu.ec>
2021-07-16 02:37:38 -0500

    version3

--- contacto.py
+++ contacto.py
@@ -1,1 +1,22 @@
-print "HOLA MUNDO"
+print ("HOLA MUNDO")
+print (2**3)
+print (6/2)
+print (6/4.)
+print (6//4)
+print (-4+8)
+print (2**2**3)
+print (2*8%5)
+
+
+ab=2
+print(ab)
+
+
+var="7"
+a="UTC"
+
+print(a+" - "+var)
+
+x=2
+print((2*x**3)-(4*x**2)+(2*x)+1)
+
